b88abbc3e8b8617d95993eca489e9900277b4cd1
Chris Goodwin <cjsgoodwin94@gmail.com>
2021-08-31 16:31:12 +1200

    Setup websocket

--- services/diceRoom/service/main.go
+++ services/diceRoom/service/main.go
@@ -1,10 +1,38 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 )
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+func socketHandler(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer c.Close()
+	for {
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", message)
+		err = c.WriteMessage(mt, message)
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
+}
 
 func main() {
 	r := gin.Default()
@@ -19,5 +47,11 @@ func main() {
 			"roomId": roomId,
 		})
 	})
+
+	// Setup websockets
+	r.GET("/room/:roomId/diceRolls/ws", func(c *gin.Context) {
+		socketHandler(c.Writer, c.Request)
+	})
+
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
