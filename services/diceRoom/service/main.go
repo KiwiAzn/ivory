f924215157472ca5523f0ed89366b816db202ddf
KiwiAzn <cjsgoodwin94@gmail.com>
2021-09-02 21:06:44 +1200

    Update dice room service to make use of chat exmaple

--- services/diceRoom/service/main.go
+++ services/diceRoom/service/main.go
@@ -1,38 +1,10 @@
 package main
 
 import (
-	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/gorilla/websocket"
 )
-
-var upgrader = websocket.Upgrader{
-	CheckOrigin: func(r *http.Request) bool { return true },
-}
-
-func socketHandler(w http.ResponseWriter, r *http.Request) {
-	c, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Print("upgrade:", err)
-		return
-	}
-	defer c.Close()
-	for {
-		mt, message, err := c.ReadMessage()
-		if err != nil {
-			log.Println("read:", err)
-			break
-		}
-		log.Printf("recv: %s", message)
-		err = c.WriteMessage(mt, message)
-		if err != nil {
-			log.Println("write:", err)
-			break
-		}
-	}
-}
 
 func main() {
 	r := gin.Default()
@@ -41,6 +13,7 @@ func main() {
 			"message": "pong",
 		})
 	})
+
 	r.GET("/room/:roomId/diceRolls", func(c *gin.Context) {
 		roomId := c.Param("roomId")
 		c.JSON(http.StatusOK, gin.H{
@@ -49,8 +22,11 @@ func main() {
 	})
 
 	// Setup websockets
+	hub := newHub()
+	go hub.run()
+
 	r.GET("/room/:roomId/diceRolls/ws", func(c *gin.Context) {
-		socketHandler(c.Writer, c.Request)
+		serveWs(hub, c.Writer, c.Request)
 	})
 
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
