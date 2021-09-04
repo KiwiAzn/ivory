7fc5707ae1384b857201f3d9515cff1ef4922689
KiwiAzn <cjsgoodwin94@gmail.com>
2021-09-04 18:51:36 +1200

    Add basic support for dice rooms

--- services/diceRoom/main.go
+++ services/diceRoom/main.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/kiwiazn/ivory/services/diceRoom/models"
 )
 
 func main() {
@@ -15,14 +16,30 @@ func main() {
 	})
 
 	// Setup websockets
-	hub := newHub()
-	go hub.run()
+	hubs := make(map[string]*Hub)
 
 	r.GET("/room/:roomId/diceRolls", func(c *gin.Context) {
-		c.JSON(http.StatusOK, hub.diceRolls)
+		roomId := c.Param("roomId")
+		hub := hubs[roomId]
+
+		if hub != nil {
+			c.JSON(http.StatusOK, hub.diceRolls)
+			return
+		}
+
+		c.JSON(http.StatusOK, []models.DiceRollWithSender{})
 	})
 
 	r.GET("/room/:roomId/diceRolls/ws", func(c *gin.Context) {
+		roomId := c.Param("roomId")
+		hub := hubs[roomId]
+
+		if hub == nil {
+			hubs[roomId] = newHub()
+			hub = hubs[roomId]
+			go hub.run()
+		}
+
 		serveWs(hub, c.Writer, c.Request)
 	})
 
