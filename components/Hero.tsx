1f098bf813db791ddd96fd69036492c1956121d4
Christopher.Goodwin <christopher.goodwin@serko.com>
2021-11-10 13:34:43 +1300

    Nostromo > Ivory

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,7 +5,7 @@ const Hero: React.FunctionComponent = () => (
   <Center>
     <Box p="8">
       <Heading as="h1" size="4xl" fontFamily="Permanent Marker">
-        Ivory
+        Nostromo
       </Heading>
       <Text fontSize="xl">
         <FormattedMessage
