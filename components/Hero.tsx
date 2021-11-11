dfa246a0b370c0516361a2540d35afc8a81f388e
Christopher.Goodwin <christopher.goodwin@serko.com>
2021-11-11 13:32:15 +1300

    Update hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,12 +5,12 @@ const Hero: React.FunctionComponent = () => (
   <Center>
     <Box p="8">
       <Heading as="h1" size="4xl" fontFamily="Permanent Marker">
-        Ivory
+        Ephemeral Demo
       </Heading>
       <Text fontSize="xl">
         <FormattedMessage
           id="hero.subTitle"
-          defaultMessage="RPG dice roller built for the web"
+          defaultMessage="I hope my demo works"
           description="Subtitle text to accompany the app name"
         />
       </Text>
