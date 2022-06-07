281cd3cd8725e0eef490d9d2779f0e04c487f49f
Christopher.Goodwin <christopher.goodwin@serko.com>
2022-06-07 14:12:58 +1200

    Add storybook dependencies

---
+++ .storybook/main.ts
@@ -0,0 +1,24 @@
+module.exports = {
+  stories: [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-webpack5",
+  },
+  webpackFinal: async (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      assert: require.resolve("assert/"),
+      util: require.resolve("util/"),
+    };
+
+    return config;
+  },
+};
