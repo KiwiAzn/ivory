2f3fddb0651d7dc09e6ab1b417c69c1893d58e59
Chris Goodwin <cjsgoodwin94@gmail.com>
2022-09-06 17:27:32 +1200

    Remove css animation (#17)
    * Remove css animations
    
    * Fix name conflict
    
    * Add missing packages
    
    * Add the other missing packages
    
    * Fix docker build
    
    Co-authored-by: Christopher.Goodwin <christopher.goodwin@serko.com>

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -38,36 +38,8 @@ const Hero: React.FunctionComponent = () => (
   <Center>
     <Dice />
     <Box ml="2" p={["4", "8"]}>
-      <Heading
-        as="h1"
-        size="4xl"
-        fontFamily="Permanent Marker"
-        sx={{
-          display: "grid",
-          gridTemplateColumns: "1fr",
-          "--stacks": "3",
-          "& span": {
-            gridRowStart: 1,
-            gridColumnStart: 1,
-            "--stack-height": "calc(100% / var(--stacks))",
-            "--inverse-index": "calc(calc(var(--stacks) - 1) - var(--index))",
-            "--clip-top": "calc(var(--stack-height) * var(--index))",
-            "--clip-bottom": "calc(var(--stack-height) * var(--inverse-index))",
-            clipPath: "inset(var(--clip-top) -10px var(--clip-bottom) -10px)",
-            animation: `${stackKeyFrames} 340ms cubic-bezier(.46,.29,0,1.24) 1 backwards calc(var(--index) * 120ms), ${glitchKeyframes} 2s ease infinite 2s alternate-reverse`,
-            userSelect: "none",
-          },
-          "& span:nth-of-type(even)": {
-            "--glitch-translate": "8px",
-          },
-          "& span:nth-of-type(odd)": {
-            "--glitch-translate": "-8px",
-          },
-        }}
-      >
-        <span style={{ "--index": 0 } as CSSProperties}>Ivory</span>
-        <span style={{ "--index": 1 } as CSSProperties}>Ivory</span>
-        <span style={{ "--index": 2 } as CSSProperties}>Ivory</span>
+      <Heading as="h1" size="4xl" fontFamily="Permanent Marker">
+        Ivory
       </Heading>
       <Text fontSize="xl">
         <FormattedMessage
