077e04c9bac801b2a1311261e1bdae5306254f34
Christopher.Goodwin <christopher.goodwin@serko.com>
2022-10-25 16:30:34 +1300

    Add missing types

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -1,52 +1,63 @@
-import * as path from "path";
 import * as pulumi from "@pulumi/pulumi";
-import * as random from "@pulumi/random";
-import * as k8s from "@pulumi/kubernetes";
+import * as containerregistry from "@pulumi/azure-native/containerregistry";
+import * as cache from "@pulumi/azure-native/cache";
+import * as resources from "@pulumi/azure-native/resources";
+import * as web from "@pulumi/azure-native/web";
+import * as path from "path";
 import * as docker from "@pulumi/docker";
-import * as cloudflare from "@pulumi/cloudflare";
 
-const branchName = process.env.BRANCH_NAME ?? "main";
+// Create an Azure Resource Group
+const resourceGroup = new resources.ResourceGroup("ivory");
 
-// Create a password
-const pass = new random.RandomPassword("pass", { length: 10 });
-
-new k8s.Provider("kubernetes-provider", {
-  cluster: "ivory_aks",
+// Create an Azure App Service Plan
+const plan = new web.AppServicePlan("plan", {
+  resourceGroupName: resourceGroup.name,
+  kind: "Linux",
+  reserved: true,
+  sku: {
+    name: "B1",
+    tier: "Basic",
+  },
 });
 
-const namespace = new k8s.core.v1.Namespace(branchName);
-
-const redis = new k8s.helm.v3.Release("redis", {
-  chart: "redis",
-  repositoryOpts: {
-    repo: "https://charts.bitnami.com/bitnami",
+const registry = new containerregistry.Registry("registry", {
+  resourceGroupName: resourceGroup.name,
+  sku: {
+    name: "Basic",
   },
-  namespace: namespace.metadata.name,
-  values: {
-    global: {
-      redis: {
-        password: pass.result,
-      },
-    },
-    rbac: {
-      create: true,
-    },
-  },
-  skipAwait: true,
+  adminUserEnabled: true,
 });
 
-const srv = k8s.core.v1.Service.get(
-  "redis-master-service",
-  pulumi.interpolate`${redis.status.namespace}/${redis.status.name}-redis-master`
+const credentials = containerregistry.listRegistryCredentialsOutput({
+  resourceGroupName: resourceGroup.name,
+  registryName: registry.name,
+});
+
+const adminUsername = credentials.apply((credentials) => credentials.username!);
+const adminPassword = credentials.apply(
+  (credentials) => credentials.passwords![0].value!
 );
 
-export const redisMasterClusterIP = srv.spec.clusterIP;
-
-const containerRegistryAuth: docker.ImageRegistry = {
-  server: "ivory.azurecr.io",
-  username: "ivory",
-  password: "C37dFUdxLF98Xv3If/q=90ajnHN6BMKW",
-};
+const redis = new cache.Redis("redis", {
+  // enableNonSslPort: true,
+  // location: "West US",
+  // minimumTlsVersion: "1.2",
+  // name: "cache1",
+  // redisConfiguration: {
+  //     maxmemoryPolicy: "allkeys-lru",
+  // },
+  // replicasPerMaster: 2,
+  resourceGroupName: resourceGroup.name,
+  // shardCount: 2,
+  sku: {
+    capacity: 1,
+    family: "P",
+    name: "Premium",
+  },
+  // staticIP: "192.168.0.5",
+  // subnetId: "/subscriptions/subid/resourceGroups/rg2/providers/Microsoft.Network/virtualNetworks/network1/subnets/subnet1",
+  // zones: ["1"],
+});
 
 const ivoryDiceRoomName = "ivory-dice-room";
 const ivoryDiceRoomAppLabels = { app: ivoryDiceRoomName };
@@ -58,57 +69,6 @@ const ivoryDiceRoomImage = new docker.Image(ivoryDiceRoomName, {
     dockerfile: path.join(__dirname, "..", "diceRoomService.Dockerfile"),
   },
   registry: containerRegistryAuth,
-});
-
-const ivoryDiceRoomDeployment = new k8s.apps.v1.Deployment(ivoryDiceRoomName, {
-  metadata: {
-    namespace: namespace.metadata.name,
-  },
-  spec: {
-    selector: { matchLabels: ivoryDiceRoomAppLabels },
-    replicas: 1,
-    template: {
-      metadata: {
-        labels: ivoryDiceRoomAppLabels,
-      },
-      spec: {
-        hostname: ivoryDiceRoomName,
-        containers: [
-          {
-            name: ivoryDiceRoomName,
-            image: ivoryDiceRoomImage.imageName,
-            ports: [{ containerPort: 8080 }],
-            env: [
-              {
-                name: "REDIS_HOST",
-                value: redisMasterClusterIP,
-              },
-              {
-                name: "REDIS_PORT",
-                value: "6379",
-              },
-              {
-                name: "REDIS_PASSWORD",
-                value: pass.result,
-              },
-            ],
-          },
-        ],
-      },
-    },
-  },
-});
-
-const ivoryDiceRoomService = new k8s.core.v1.Service(ivoryDiceRoomName, {
-  metadata: {
-    labels: ivoryDiceRoomDeployment.spec.template.metadata.labels,
-    namespace: namespace.metadata.name,
-  },
-  spec: {
-    type: "ClusterIP",
-    ports: [{ port: 8080, targetPort: 8080, protocol: "TCP" }],
-    selector: ivoryDiceRoomAppLabels,
-  },
 });
 
 const ivoryUiName = "ivory-ui";
@@ -123,152 +83,90 @@ const ivoryUiImage = new docker.Image(ivoryUiName, {
   registry: containerRegistryAuth,
 });
 
-const ivoryUiDeployment = new k8s.apps.v1.Deployment(ivoryUiName, {
-  metadata: {
-    namespace: namespace.metadata.name,
-  },
-  spec: {
-    selector: { matchLabels: ivoryAppLabels },
-    replicas: 1,
-    template: {
-      metadata: {
-        labels: ivoryAppLabels,
-      },
-      spec: {
-        hostname: ivoryUiName,
-        containers: [
-          {
-            name: ivoryUiName,
-            image: ivoryUiImage.imageName,
-            ports: [{ containerPort: 3000 }],
-            env: [
-              {
-                name: "BACKEND_HOST",
-                value: ivoryDiceRoomService.spec.clusterIP,
-              },
-              {
-                name: "BACKEND_PORT",
-                value: "8080",
-              },
-            ],
-          },
-        ],
-      },
-    },
-  },
-});
-
-const ivoryUiServer = new k8s.core.v1.Service(ivoryUiName, {
-  metadata: {
-    labels: ivoryUiDeployment.spec.template.metadata.labels,
-    namespace: namespace.metadata.name,
-  },
-  spec: {
-    type: "ClusterIP",
-    ports: [{ port: 3000, targetPort: 3000, protocol: "TCP" }],
-    selector: ivoryAppLabels,
-  },
-});
-
-const ruleHost =
-  branchName !== "main" ? `${branchName}.ivorydice.online` : "ivorydice.online";
-
-const ingressName = "ingress";
-const ingress = new k8s.networking.v1.Ingress(ingressName, {
-  metadata: {
-    annotations: {
-      "kubernetes.io/ingress.class": "nginx",
-      "nginx.ingress.kubernetes.io/use-regex": "true",
-      "nginx.ingress.kubernetes.io/rewrite-target": "/$1",
-      "nginx.ingress.kubernetes.io/enable-rewrite-log": "true",
-    },
-    namespace: namespace.metadata.name,
-  },
-  spec: {
-    defaultBackend: {
-      service: {
-        name: ivoryUiServer.metadata.name,
-        port: {
-          number: 3000,
-        },
-      },
-    },
-    rules: [
+const ivoryUiApp = new web.WebApp(ivoryUiName, {
+  resourceGroupName: resourceGroup.name,
+  serverFarmId: plan.id,
+  siteConfig: {
+    appSettings: [
       {
-        host: ruleHost,
-        http: {
-          paths: [
-            {
-              path: "/api/(.*)",
-              pathType: "Prefix",
-              backend: {
-                service: {
-                  name: ivoryDiceRoomService.metadata.name,
-                  port: {
-                    number: 8080,
-                  },
-                },
-              },
-            },
-          ],
-        },
+        name: "WEBSITES_ENABLE_APP_SERVICE_STORAGE",
+        value: "false",
       },
       {
-        host: ruleHost,
-        http: {
-          paths: [
-            {
-              path: "/(.*)",
-              pathType: "Prefix",
-              backend: {
-                service: {
-                  name: ivoryUiServer.metadata.name,
-                  port: {
-                    number: 3000,
-                  },
-                },
-              },
-            },
-          ],
-        },
+        name: "DOCKER_REGISTRY_SERVER_URL",
+        value: pulumi.interpolate`https://${registry.loginServer}`,
+      },
+      {
+        name: "DOCKER_REGISTRY_SERVER_USERNAME",
+        value: adminUsername,
+      },
+      {
+        name: "DOCKER_REGISTRY_SERVER_PASSWORD",
+        value: adminPassword,
+      },
+      {
+        name: "WEBSITES_PORT",
+        value: "80", // Our custom image exposes port 80. Adjust for your app as needed.
+      },
+      {
+        name: "BACKEND_HOST",
+        value: ivoryServerApp.defaultHostName,
+      },
+      {
+        name: "BACKEND_PORT",
+        value: "8080",
       },
     ],
+    alwaysOn: true,
+    linuxFxVersion: pulumi.interpolate`DOCKER|${ivoryUiImage.imageName}`,
   },
+  httpsOnly: true,
 });
 
-const nginx = new k8s.helm.v3.Release("nginx-ingress", {
-  chart: "nginx-ingress",
-  repositoryOpts: {
-    repo: "https://charts.helm.sh/stable/",
-  },
-  namespace: namespace.metadata.name,
-});
+export const ivoryUiAppEndpoint = pulumi.interpolate`https://${ivoryUiApp.defaultHostName}`;
 
-const nginxService = k8s.core.v1.Service.get(
-  "nginx-ingress-controller",
-  pulumi.interpolate`${nginx.status.namespace}/${nginx.status.name}-nginx-ingress-controller`
-);
-
-export const publicIp = nginxService.status.loadBalancer.ingress[0].ip;
-
-let record;
-
-if (branchName !== "main") {
-  const ivoryDiceAppZone = pulumi.output(
-    cloudflare.getZones({
-      filter: {
-        name: "ivorydice.online",
+const ivoryServerApp = new web.WebApp(ivoryDiceRoomName, {
+  resourceGroupName: resourceGroup.name,
+  serverFarmId: plan.id,
+  siteConfig: {
+    appSettings: [
+      {
+        name: "WEBSITES_ENABLE_APP_SERVICE_STORAGE",
+        value: "false",
       },
-    })
-  );
+      {
+        name: "DOCKER_REGISTRY_SERVER_URL",
+        value: pulumi.interpolate`https://${registry.loginServer}`,
+      },
+      {
+        name: "DOCKER_REGISTRY_SERVER_USERNAME",
+        value: adminUsername,
+      },
+      {
+        name: "DOCKER_REGISTRY_SERVER_PASSWORD",
+        value: adminPassword,
+      },
+      {
+        name: "WEBSITES_PORT",
+        value: "80", // Our custom image exposes port 80. Adjust for your app as needed.
+      },
+      {
+        name: "REDIS_HOST",
+        value: redis.hostName,
+      },
+      {
+        name: "REDIS_PORT",
+        value: redis.port,
+      },
+      {
+        name: "REDIS_PASSWORD",
+        value: redis.accessKeys.primaryKey,
+      },
+    ],
+    alwaysOn: true,
+    linuxFxVersion: pulumi.interpolate`DOCKER|${ivoryDiceRoomImage.imageName}`,
+  },
+  httpsOnly: true,
+});
 
-  record = new cloudflare.Record("record", {
-    name: branchName,
-    zoneId: ivoryDiceAppZone.zones[0].id as pulumi.Input<string>,
-    type: "A",
-    value: publicIp,
-    proxied: true,
-  });
-}
-
-export const hostname = record?.hostname ?? "https://ivorydice.online";
+export const ivoryServerAppEndpoint = pulumi.interpolate`https://${ivoryServerApp.defaultHostName}`;
